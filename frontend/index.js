479e4378dc6e73df79bf85a089e3c65f3482317c
Candice Peters <candice.peters@flatironschool.com>
2021-09-06 19:50:51 -0500

    adding js changes before lecture

--- frontend/index.js
+++ frontend/index.js
@@ -1,17 +1,33 @@
+
 // make a fetch request to the '/items' and display items on the DOM
+
+const ul = document.getElementById('item-list');
+const form = document.getElementById('item-form');
+const priceInput = document.getElementById('item-price');
+const nameInput = document.getElementById('item-name');
+const descriptionInput = document.getElementById('item-description');
+
 fetch('http://localhost:3000/items')
 .then(r => r.json())
 .then(data => renderItems(data))
 
 function renderItems(data){
     const items = data["data"];
-    const ul = document.getElementById('item-list');
     items.forEach((item) => {
-        const li = document.createElement('li');
-        li.innerText = `${item["attributes"]["name"]} - ${item["attributes"]["price"]}`
-        ul.appendChild(li)
+        
         // debugger
-
+        renderItem(item)
     })
+
+    function renderItem(item){
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <div>
+       $<span class="price">${item["attributes"]["price"]}</span>
+        <span class="name">${item["attributes"]["name"]}</span>
+        <span class="description">${item["attributes"]["description"]}</span>
+       </div>`
+        ul.appendChild(li)
+    }
     // debugger
 }
