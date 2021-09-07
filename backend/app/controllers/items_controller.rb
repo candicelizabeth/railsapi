cbc80c82b1432e344523c48b748d39fdd5d43c61
Candice Peters <candice.peters@flatironschool.com>
2021-09-07 18:26:48 -0500

    after lecture

--- backend/app/controllers/items_controller.rb
+++ backend/app/controllers/items_controller.rb
@@ -9,4 +9,27 @@ class ItemsController < ApplicationController
         item = Item.find_by_id(params[:id]) 
         render json: item.to_json(except: [:created_at, :updated_at], include: {category: {only: [:name]}})
     end
+
+    def create 
+        item = Item.new(item_params)
+        item.category = Category.last ##NEEDS TO CHANGE!!!!!
+        if item.save 
+            render json: ItemSerializer.new(item)
+        else
+            render json: {error: "Couldn't save"}
+        end
+    end
+
+    def destroy 
+        item = Item.find_by_id(params[:id])
+        item.destroy 
+        render json: {message: "Successfully deleted #{item.name}"}
+    end
+
+
+    private 
+    
+    def item_params 
+        params.require(:item).permit(:name, :description, :price)
+    end
 end
--- frontend/index.js
+++ frontend/index.js
@@ -6,10 +6,36 @@ const form = document.getElementById('item-form');
 const priceInput = document.getElementById('item-price');
 const nameInput = document.getElementById('item-name');
 const descriptionInput = document.getElementById('item-description');
+const baseURL = 'http://localhost:3000/items'
+form.addEventListener('submit', handleSubmit)
 
-fetch('http://localhost:3000/items')
+function handleSubmit(e){
+    // debugger
+    const itemInfo = {
+        price: priceInput.value,
+        name: nameInput.value,
+        description: descriptionInput.value
+    }
+    // debugger
+    e.preventDefault();
+    const configObject = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(itemInfo)
+    }
+    fetch(baseURL, configObject)
+    .then(r => r.json())
+    .then(json => renderItem(json["data"]))
+}
+
+function getItems(){
+fetch(baseURL)
 .then(r => r.json())
 .then(data => renderItems(data))
+}
 
 function renderItems(data){
     const items = data["data"];
@@ -19,15 +45,43 @@ function renderItems(data){
         renderItem(item)
     })
 
-    function renderItem(item){
-        const li = document.createElement('li');
-        li.innerHTML = `
-        <div>
-       $<span class="price">${item["attributes"]["price"]}</span>
-        <span class="name">${item["attributes"]["name"]}</span>
-        <span class="description">${item["attributes"]["description"]}</span>
-       </div>`
-        ul.appendChild(li)
-    }
+    
     // debugger
 }
+function renderItem(item){
+
+    const li = document.createElement('li');
+    // debugger
+    li.innerHTML = `
+    <div>
+   $<span class="price">${item["attributes"]["price"]}</span>
+    <span class="name">${item["attributes"]["name"]}</span>
+    <span class="description">${item["attributes"]["description"]}</span>
+   </div>
+   <button class="edit" data-id= "${item.id}" >Edit</button>
+   <button class="delete" data-id= "${item.id}" >Delete</button>
+   `
+    ul.appendChild(li)
+    form.reset()
+    li.addEventListener('click', handleClick)
+}
+
+function handleClick(e){
+    // debugger 
+    if(e.target.innerText === "Delete"){
+        // debugger
+        const id = e.target.dataset.id 
+        e.target.parentElement.remove()
+        const configObject = {
+            method: 'DELETE'
+        }
+        // debugger
+        fetch(baseURL + `/${id}`, configObject)
+        .then(r => r.json())
+        .then(json => alert(json.message))
+
+    }else if(e.target.innerText === "Edit"){
+
+    }
+}
+getItems()
