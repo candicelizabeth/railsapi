89efa82635c4f4b1241cd682f463052e9f073abb
Candice Peters <candice.peters@flatironschool.com>
2021-09-21 08:26:00 -0500

    changes before lecture

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,8 @@
 // make a fetch request to the '/items' and display items on the DOM
 const port = 'http://localhost:3000';
 const itemApi = new ItemApi(port);
+const categoryApi = new CategoryApi(port);
+const dropdown = document.getElementById('cat-dropdown')
 // debugger
 const ul = document.getElementById('item-list');
 const form = document.getElementById('item-form');
@@ -74,3 +76,4 @@ function saveUpdatedItem(saveBtn){
 }
 
 itemApi.getItems()
+categoryApi.getCategories()
